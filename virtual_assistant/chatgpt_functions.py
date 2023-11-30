31be7208395a36270d0666cbc0fcd1bcb2611829
Алексей Ватлин <vatlin04@mail.ru>
2023-11-30 21:38:55 +0500

    Переписал на работу с оригинальным chatgpt

--- virtual_assistant/chatgpt_functions.py
+++ virtual_assistant/chatgpt_functions.py
@@ -1,9 +1,46 @@
+import os
 import g4f
+import httpx
 from langdetect import detect
+from openai import OpenAI
 
 number_similar_books = 7
 
+# Работает с пересказом текстов, но только по ссылке, я не нашел как перестроить это на текст
+# def yandexGpt (text30k):
+#     import requests
+#     endpoint = 'https://300.ya.ru/api/sharing-url'
+#     response = requests.post(
+#         endpoint,
+#         json={
+#             'article_url': 'https://habr.com/ru/news/729422/'
+#         },
+#         headers={'Authorization': 'OAuth !token!'}
+#     )
+#
+#     print(response.text)
+
 def get_response(message):
+    api_key = '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'
+    os.environ["OPENAI_API_KEY"] = api_key
+    client = OpenAI(
+        http_client=httpx.Client(
+        proxies="http://RFk9wC:Q8AS8q@46.232.14.84:8000",
+        ),
+    )
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+            "role": "user",
+             "content": message
+            }
+        ],
+    )
+    return response.choices[0].message.content
+
+
+def get_response2(message):
     _providers = [
         g4f.Provider.GeekGpt,
         g4f.Provider.ChatBase,
@@ -50,8 +87,20 @@ def get_similar_books(book_name):
 '''
 def explain_term(term):
     if (len(term.split()) > 2):
-        return get_response("Объясни значение фразы" + term)
-    return get_response("Объясни значение слова" + term)
+        return get_response("Объясни значение фразы " + term)
+    return get_response("Объясни значение слова " + term)
 
-print(get_similar_books("Приключение барона Мюнхгаузена"))
+def get_book_info(book_name):
+    return get_response("Приведи только достоверные исторические факты о книге " + book_name)
 
+def get_book_analogies(book_name):
+    return get_response("Приведи список только реально существующих фильмов и пьес на основе книги " + book_name)
+
+def retell_text(text_8k_limited):
+    try:
+        return get_response("Перескажи текст:\n" + text_8k_limited)
+    except:
+        return "Текст слишном большой для пересказа, для лучшего понимания советуем прочитать его самостоятельно)"
+
+print(get_book_info("Капитанская дочка"))
+print(get_book_analogies("Капитанская дочка"))
